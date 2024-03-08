dbe3cf4bfd5824140a647feab4ca4eaa6b86ee8a
mrendyg <andyg.oyarzo@gmail.com>
2024-03-08 15:26:05 -0300

    avances con menu responsivo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 import React from "react"
 import logoargom from '../assets/logoargom.png'
+import logoinstagram from '../IMG/logos/greeninstagram.png'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
@@ -9,7 +10,7 @@ const Header = () => {
     return (
 
       <>
-        <Disclosure as="nav" className="bg-[#f2f2f2] w-auto h-[6rem] sm:h-[16rem]  sm:items-stretch sm:justify-start ">
+        <Disclosure as="nav" className="bg-[#f2f2f2] w-auto h-[5rem] sm:h-[16rem]  sm:items-stretch sm:justify-start ">
           {({open}) =>
             <>
               <div className="mx-auto max-w-7x1 px-2 sm:px-6 lg:px-8">
@@ -17,11 +18,12 @@ const Header = () => {
                   <div className="absolute inset-y-0 left-0 flexditems-center sm:hidden">
 
                     <Disclosure.Button className="inline-flex items-center justify-center m rounded-md p-2 text-[#000000] hover:text-[#AEAEAE]">
+                    <img src={logoargom} alt="" width={50}/>
                       <span className="sr-only">Open main menu</span>
                       {open ? (
                         <XMarkIcon className="block h-[3rem] w-[3rem]" aria-hidden="true" />
                       ) : (
-                        <Bars3Icon className="block h-[3rem] w-[rem]" aria-hidden="true"/>
+                        <Bars3Icon className="block h-[3rem] w-[3rem]" aria-hidden="true"/>
                       )}
                     </Disclosure.Button>
 
@@ -75,10 +77,46 @@ const Header = () => {
 
                 </div>
 
-                <Disclosure.Panel className="sm:hidden">
+                <Disclosure.Panel className="sm:hidden w-auto h-screen bg-[#f2f2f2] p-[12rem] mt-[20rem]">
 
-                <div className="mt-4 bg-gray-200 p-4 lg:hidden">
-                  <p>Este es un panel normal que solo se muestra en dispositivos pequeños y medianos.</p>
+                <div className="mt-4 p-4 lg:hidden]">
+
+                  
+
+                  <div className="w-screen grid grid-cols-1 b-[2rem">
+                 
+                  <Link
+                    to={'/'}
+                    className='font-serif text-[2rem] p-2 m-[1rem] px-4 rounded-lg text-black'
+                  >
+                    Inicio
+                  </Link>
+                  <Link
+                    to={'/'}
+                    className='font-serif text-[2rem] p-2 m-[1rem] px-4 rounded-lg text-black'
+                  >
+                    Catalogo
+                  </Link>
+                  <Link
+                    to={'/'}
+                    className='font-serif text-[2rem] p-2 m-[1rem] px-4 rounded-lg text-black'
+                  >
+                    Sobre Nosotros
+                  </Link>
+                  <Link
+                    to={'/'}
+                    className='font-serif text-[2rem] p-2 m-[1rem] px-4 rounded-lg text-black'
+                  >
+                    Contacto
+                  </Link>
+
+                  <Link
+                    to="https://www.instagram.com/argom_delicias?igsh=MzZzYzJxanpudnFr"
+                    className=""
+                  >
+                   <img src={logoinstagram} alt="logoinstagram" width={50} height={50} className="ml-[2rem] mt-[1rem]"/>
+                  </Link>
+                  </div>
                 </div>
                 </Disclosure.Panel>
                           
@@ -88,50 +126,6 @@ const Header = () => {
           }
         </Disclosure>
 
-
-
-
-        {/* <Disclosure as="nav" className="bg-grey dark:bg-gray-800">
-          
-            <div className="bg-[#f2f2f2] w-auto h-[16rem]  sm:items-stretch sm:justify-start ">
-                <div className="flex justify-center">
-                <Link
-                  to={'/'}
-                  className="font-serif pt-[8rem] m-[4rem] font-size text-[1.6rem]"
-                >
-                    Inicio
-                </Link>
-                <Link
-                  to={'/'}
-                  className="font-serif pt-[8rem] m-[4rem] font-size text-[1.6rem]"
-                >
-                    Catalogo
-                </Link>
-
-                <Link 
-                  to={'/'}
-                  className="w-[12.5rem] h-[12.5rem] mt-[2rem] m-[2rem]"
-                >
-                    <img src={logoargom} alt="logoargom"/>
-                </Link>
-
-                <Link
-                  to={'/'}
-                  className="font-serif pt-[8rem] m-[4rem] font-size text-[1.6rem]"
-                >
-                    Sobre Nosotros
-                </Link>
-
-                <Link
-                  to={'/'}
-                  className="font-serif pt-[8rem] m-[4rem] font-size text-[1.6rem]"
-                >
-                    Contacto
-                </Link>
-                </div>
-
-            </div>
-            </Disclosure> */}
         </>
 
     )
