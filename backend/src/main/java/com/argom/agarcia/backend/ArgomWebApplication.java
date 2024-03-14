850b377c5e74c656f307774ce12c96cf5ede9006
AndyGarcia <andyg.oyarzo@gmail.com>
2024-03-13 22:14:34 -0300

    Spring añadido

---
+++ backend/src/main/java/com/argom/agarcia/backend/ArgomWebApplication.java
@@ -0,0 +1,13 @@
+package com.argom.agarcia.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgomWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgomWebApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/argom/agarcia/backend/ArgomWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argom.agarcia.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgomWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
